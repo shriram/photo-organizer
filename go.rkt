46e0e42344fea44bbc6c9ca696d46e1def27d147
Shriram Krishnamurthi <sk@cs.brown.edu>
2020-05-18 20:21:12 -0400

    Fixed regexp. Kept around a few useful filenames for future testing.

--- go.rkt
+++ go.rkt
@@ -4,7 +4,11 @@
 
 (define all-files/path (directory-list target-dir))
 
-(define fn-r #rx"(....)-(..)-(..) (..)\\.(..)\\.(..)\\..*")
+;; Example filenames:
+(define fn/1 "2019-04-29 20.32.56.jpg")
+(define fn/2 "2020-04-25 11.00.14-2.jpg")
+(define fn/3 "2020-02-15 13.09.23.mp4")
+(define fn-r #rx"(....)-(..)-(..) (..)\\.(..)\\.(..).*")
 
 ;; Print filenames that don't match
 ;; Most should be harmless (like .DS_Store and Icon)
