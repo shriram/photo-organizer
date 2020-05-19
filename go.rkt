ca881f794d4539f73c3f5c8b99a867f493515975
Shriram Krishnamurthi <sk@cs.brown.edu>
2020-05-18 20:19:34 -0400

    Helper to tell us which files are not being caught by the regexp. We should expect a few (like OS metadata) but not any real files. Oops.

--- go.rkt
+++ go.rkt
@@ -5,3 +5,10 @@
 (define all-files/path (directory-list target-dir))
 
 (define fn-r #rx"(....)-(..)-(..) (..)\\.(..)\\.(..)\\..*")
+
+;; Print filenames that don't match
+;; Most should be harmless (like .DS_Store and Icon)
+;;   but run this to make sure no important files are missed
+(define (non-matching)
+  (filter (λ (f) (not (regexp-match fn-r (path->string f))))
+          all-files/path))
